1b6c6b8d9d004e51a64933f3cb5482305a6f9095
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-07-27 22:30:05 +0200

    Initial ft_boolean header file commit

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,15 @@
+#ifndef FT_BOOLEAN_H
+
+# define FT_BOOLEAN_H
+# define TRUE 1
+# define FALSE 0
+# define EVEN(num) (num % 2)
+# define EVEN_MSG "I have an even number of arguments\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS "0"
+
+#include <unistd.h>
+
+typedef int t_bool;
+
+#endif
